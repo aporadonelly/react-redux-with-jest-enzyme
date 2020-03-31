c38b7c14fc03d051ff54f24ae1946db14ea38471
aporadonelly <aporado.nellyrose@gmail.com>
2020-03-31 17:38:51 +0800

    Added redux

---
+++ src/store.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './reducers';
+// import { create } from 'react-test-renderer';
+import thunk from 'redux-thunk';
+
+export const middlewares = [thunk];
+
+export const createStoreWithMiddleware = applyMiddleware(...middlewares)(
+  createStore
+);
+
+export const store = createStoreWithMiddleware(rootReducer);
+
+export default store;
