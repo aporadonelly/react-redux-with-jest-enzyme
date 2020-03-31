849b3180734fd7447614e36556f1ed033a51bd20
aporadonelly <aporado.nellyrose@gmail.com>
2020-03-31 17:00:17 +0800

    Header test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './app.scss';
 import Header from './component/header';
+import Headline from './component/headline';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
         <Header />
+        <section className='main'>
+          <Headline header='Posts' desc='Click me' />
+        </section>
       </div>
     );
   }
